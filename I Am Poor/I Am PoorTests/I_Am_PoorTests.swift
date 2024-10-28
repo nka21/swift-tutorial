334298634ca76f0f9187fed5a0a53a5847476d31
nka21 <naoki.nico@gmail.com>
2024-10-28 20:46:09 +0900

    I am Rich Appicon 追加

---
+++ I Am Poor/I Am PoorTests/I_Am_PoorTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_PoorTests.swift
+//  I Am PoorTests
+//
+//  Created by nk21 on 2024/10/28.
+//
+
+import Testing
+@testable import I_Am_Poor
+
+struct I_Am_PoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
